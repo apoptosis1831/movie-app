a6f99d5e70ea84541a45ee16a0e389e94134214a
apoptosis1831 <apoptosis1831@gmail.com>
2022-06-11 17:46:34 +0900

    make favoritePage (complete project)

--- client/src/components/views/FavoritePage/FavoritePage.js
+++ client/src/components/views/FavoritePage/FavoritePage.js
@@ -1,19 +1,62 @@
 import Axios from 'axios'
-import React, { useEffect } from 'react'
+import React, { useEffect , useState} from 'react'
 import './favorite.css'
+import { Popover} from 'antd'
+import { IMAGE_BASE_URL } from '../../Config'
 
 function FavoritePage() {
-    useEffect(()=>{
+    const [Favorites, setFavorites] = useState([])
+    const fetchFavoredMovie = () =>{
         Axios.post('/api/favorite/getFavoritedMovie',{userFrom : localStorage.getItem('userId')})
         .then(response=>{
             if(response.data.success){
-
+                setFavorites(response.data.favorites)
             } else{
                 alert('영화 정보를 가져오는데 실패했습니다');
             }
         })
-        
+    }
+
+    useEffect(()=>{
+        fetchFavoredMovie()
     }, [])
+
+    
+    const onClickDelete = (movieId, userFrom) =>{
+        const variables = {
+            movieId,
+            userFrom
+        }
+
+        Axios.post('/api/favorite/removeFromFavorite', variables)
+        .then(response=>{
+            if(response.data.success){
+                fetchFavoredMovie();
+            } else {
+                alert("리스트에서 지우는데 실패했습니다");
+            }
+        })
+    }
+
+
+    const renderCards = Favorites.map((favorite, index)=>{
+        const content=(
+            <div>
+                {favorite.moviePost ?
+                    <img src={`${IMAGE_BASE_URL}w500${favorite.moviePost} `} /> : "no image"
+                }
+            </div>
+        )
+        return <tr key = {index}>
+            <Popover content={content} title={`${favorite.movieTitle}`}>
+            <td>{favorite.movieTitle}</td>
+            </Popover>
+            <td>{favorite.movieRunTime} mins</td>
+            <td><button onClick={() => onClickDelete(favorite.movieId,favorite.userFrom)}>Remove</button></td>
+        </tr>
+    })
+
+
   return (
     <div style = {{ width : '85%' , margin : '3rem auto'}}>
         <h2> Favorite Movies</h2>    
@@ -30,9 +73,9 @@ function FavoritePage() {
         
         
             <tbody>
-
-
-
+            
+            {renderCards}
+            
             </tbody>
         
         </table>
